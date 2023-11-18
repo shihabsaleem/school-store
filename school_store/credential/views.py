a0218a3ed134d4ce8c02ffa9c3730d0d37f208fa
shihabsaleem <shihabchikkushihab@gmail.com>
2023-11-18 17:01:01 +0530

    credentials

--- school_store/credential/views.py
+++ school_store/credential/views.py
@@ -1,9 +1,8 @@
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
-
-# Create your views here.
 def register(request):
     if request.method == 'POST':
         uname = request.POST['uname']
@@ -12,35 +11,33 @@ def register(request):
 
         if pwd == cpwd:
             if User.objects.filter(username=uname).exists():
-                messages.info(request, "username already taken")
+                messages.info(request, "Username already taken")
                 return redirect('register')
             else:
                 usr = User.objects.create_user(username=uname, password=pwd)
                 usr.save()
-                print("user created !!")
+                messages.success(request, "User created successfully!")
                 return redirect('login')
         else:
-            print("error in user creation...")
+            messages.error(request, "Passwords do not match")
             return redirect('register')
-        return redirect('/')
     return render(request, "register.html")
-
 
 def login(request):
     if request.method == 'POST':
         uname = request.POST['username']
         pwd = request.POST['password']
 
-        user = auth.authenticate(username=uname,password=pwd)
+        user = authenticate(username=uname, password=pwd)
 
         if user is not None:
-            auth.login(request,user)
+            auth_login(request, user)
             return redirect('/')
         else:
-            messages.info(request,'invalid credentials')
+            messages.error(request, 'Invalid credentials')
             return redirect('login')
     return render(request, "login.html")
 
 def logout(request):
-    auth.logout(request)
+    auth_logout(request)
     return redirect('/')
