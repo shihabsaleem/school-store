2e990398da70e2703009522f93f7197a6da7e059
shihabsaleem <shihabchikkushihab@gmail.com>
2023-11-18 23:25:15 +0530

    model -dept

--- school_store/homepage/models.py
+++ school_store/homepage/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=75, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    desc = models.TextField(blank=True)
+    img = models.ImageField(upload_to='dept', blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'department'
+        verbose_name_plural = 'departments'
+
+    def get_url(self):
+        return reverse('homepage:dept', args=[self.slug])
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+class Course(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    dept = models.ForeignKey(Department, on_delete=models.CASCADE)
+    seats = models.IntegerField()
+
+    def get_url(self):
+        return reverse('homepage:ProCatDetail', args=[self.dept.slug, self.slug])
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'course'
+        verbose_name_plural = 'courses'
+
+    def __str__(self):
+        return '{}'.format(self.name)
--- school_store/homepage/views.py
+++ school_store/homepage/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Department, Course
 
-# Create your views here.
+def dept(request, dept_slug=None):
+    dept_page = None
+    course_list = None
+    department = Department.objects.all()
+
+    if dept_slug is not None:
+        dept_page = get_object_or_404(Department, slug=dept_slug)
+        course_list = Course.objects.filter(category=dept_page, available=True)
+    else:
+        course_list = Course.objects.filter(available=True)
+
+    return render(request, 'dept.html', {'dept_page': dept_page, 'department': department})
+
 def home(request):
     return render(request, "index.html")
+
+
+def course(request, dept_slug, course_slug):
+    try:
+        product = Course.objects.get(dept__slug=dept_slug, slug=course_slug)
+    except Exception as e:
+        raise e
+
+    return render(request, 'dept.html', {'product': product})
+
