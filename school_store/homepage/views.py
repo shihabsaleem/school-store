a9d5da5970ffa0e7d2b7a81913882a879abed223
shihabsaleem <shihabchikkushihab@gmail.com>
2023-11-19 13:29:29 +0530

    form

--- school_store/homepage/views.py
+++ school_store/homepage/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Department
 
+
 def dept(request, dept_slug=None):
     department = Department.objects.all()
 
@@ -12,6 +13,10 @@ def dept(request, dept_slug=None):
 
     return render(request, 'dept.html', {'dept_page': dept_page, 'department': department})
 
+
 def home(request):
     return render(request, "index.html")
 
+
+def form(request):
+    return render(request, 'form.html')
